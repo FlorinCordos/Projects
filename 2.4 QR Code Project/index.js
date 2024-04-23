557539941b26b18311a5481c20f22306b3648876
FlorinCordos <85672044+FlorinCordos@users.noreply.github.com>
2024-04-23 20:51:18 +0300

    Update index.js

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -21,9 +21,9 @@ inquirer
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
+     
     } else {
-      // Something else went wrong
+    
     }
   });
 
